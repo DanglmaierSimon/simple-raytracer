9442118d6270acc57585663aa9933a3584d1ceb1
Simon Danglmaier <simon.danglmaier@sequality.at>
2020-04-20 22:19:09 +0200

    Remove unused function

--- ray.h
+++ ray.h
@@ -28,20 +28,3 @@ class ray {
     vec3 _origin;
     vec3 _dir;
 };
-
-double hit_sphere(vec3 const& center, double const radius, ray const& r)
-{
-    vec3 oc = r.origin() - center;
-
-    auto a      = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c      = oc.length_squared() - radius * radius;
-
-    auto discriminant = half_b * half_b - a * c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    }
-
-    return (-half_b - sqrt(discriminant)) / a;
-}
