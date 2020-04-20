f41c6507d99675c4a9b339ecf0d775bb74f126b4
Simon Danglmaier <simon.danglmaier@sequality.at>
2020-04-20 21:41:58 +0200

    Use c++ 11 random library

--- util.h
+++ util.h
@@ -2,8 +2,10 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <functional>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 
@@ -33,8 +35,13 @@ constexpr inline double ffmax(double a, double b)
 
 inline double random_double()
 {
-    return rand() / (RAND_MAX + 1.0);
+    static std::random_device                     rand_dev;
+    static std::mt19937                           generator(rand_dev());
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+    return distribution(generator);
 }
+
 inline double random_double(double min, double max)
 {
     return min + (max - min) * random_double();
