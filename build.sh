af09b328d2c4a452d96d72c9fc301f9dc9e8d32a
Simon Danglmaier <simon.danglmaier@sequality.at>
2020-04-21 22:37:31 +0200

    Update build and run scripts

--- build.sh
+++ build.sh
@@ -3,7 +3,35 @@
 set -o errexit
 set -o pipefail
 
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Color Constants
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+print_info() {
+    echo -e "${BLUE}$1${NC}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}$1${NC}"
+}
+
+print_error() {
+    echo -e "${RED}$1${NC}"
+}
+
+print_success() {
+    echo -e "${GREEN}$1${NC}"
+}
+
 BUILD_TYPE="DEBUG"
+
+print_info "Starting build script..."
 
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -22,11 +50,19 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+print_info "Build Type: $BUILD_TYPE"
+
 OUTPUT_DIR="build"
 DEFAULT_TARGET="raytracer"
 
-cd $OUTPUT_DIR || exit -1
+cd "$__dir"/"$OUTPUT_DIR" || exit -1
+
+print_info "Running cmake..."
 
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
+print_info "Building target $DEFAULT_TARGET..."
+
 make $DEFAULT_TARGET -j 4
+
+print_success "==== BUILD SCRIPT SUCCESSFUL ===="
--- run.sh
+++ run.sh
@@ -3,12 +3,46 @@
 set -o errexit
 set -o pipefail
 
-./build.sh --release
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd build || exit -1
+# Color Constants
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+print_info() {
+    echo -e "${BLUE}$1${NC}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}$1${NC}"
+}
+
+print_error() {
+    echo -e "${RED}$1${NC}"
+}
+
+print_success() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+print_info "Building project in release mode..."
+
+"$__dir/build.sh" --release
+
+cd "$__dir/build" || exit -1
+
+print_info "Removing old image..."
 
 rm -f image.ppm
 
-./raytracer >>image.ppm
+print_info "Generating image..."
 
-gwenview image.ppm
+"$__dir/build/raytracer"
+
+print_success "Image created successfully!"
+
+gwenview image.ppm &
