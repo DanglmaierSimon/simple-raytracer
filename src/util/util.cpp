d632a3c0558251c620c539cd5bbc666f9721030f
Simon Danglmaier <S1610306004@students.fh-hagenberg.at>
2021-02-11 20:39:19 +0100

    Add util namespace

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -2,25 +2,31 @@
 
 #include <random>
 
-std::string format_time(std::chrono::nanoseconds time)
+std::string util::format_time(std::chrono::nanoseconds time)
 {
     using namespace std::chrono;
 
-    auto s_str = std::to_string(duration_cast<seconds>(time).count());
+    const auto s_str = std::to_string(duration_cast<seconds>(time).count());
 
-    auto ms_str =
-        left_pad_string(std::to_string(duration_cast<milliseconds>(time).count() % 1000), 3, '0');
+    const auto ms_str =
+        util::left_pad_string(std::to_string(duration_cast<milliseconds>(time).count() % 1000),
+                              3,
+                              '0');
 
-    auto mcs_str =
-        left_pad_string(std::to_string(duration_cast<microseconds>(time).count() % 1000), 3, '0');
+    const auto mcs_str =
+        util::left_pad_string(std::to_string(duration_cast<microseconds>(time).count() % 1000),
+                              3,
+                              '0');
 
-    auto ns_str =
-        left_pad_string(std::to_string(duration_cast<nanoseconds>(time).count() % 1000), 3, '0');
+    const auto ns_str =
+        util::left_pad_string(std::to_string(duration_cast<nanoseconds>(time).count() % 1000),
+                              3,
+                              '0');
 
     return (s_str == "0" ? "" : s_str + "s ") + ms_str + "ms " + mcs_str + "µs " + ns_str + "ns";
 }
 
-std::string left_pad_string(std::string str, unsigned int length, char pad_char)
+std::string util::left_pad_string(std::string str, unsigned int length, char pad_char)
 {
     if (length < str.size()) {
         return str;
@@ -29,17 +35,17 @@ std::string left_pad_string(std::string str, unsigned int length, char pad_char)
     return str.insert(0, length - str.size(), pad_char);
 }
 
-std::string format_time(long long time)
+std::string util::format_time(long long time)
 {
     return format_time(static_cast<std::chrono::nanoseconds>(time));
 }
 
-double random_double(double min, double max)
+double util::random_double(double min, double max)
 {
-    return min + (max - min) * random_double();
+    return min + (max - min) * util::random_double();
 }
 
-double random_double()
+double util::random_double()
 {
     static std::random_device                     rand_dev;
     static std::mt19937                           generator(rand_dev());
--- src/util/util.h
+++ src/util/util.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
 #include <chrono>
 #include <cmath>
 #include <cstdlib>
+#include <limits>
 #include <memory>
 #include <string>
 
@@ -11,32 +13,41 @@ class vec3;
 class ray;
 class hittable;
 
-// Usings
-using std::make_shared;
-using std::shared_ptr;
+namespace util {
 
 // Constants
-constexpr double pi = M_PI;
+constexpr double PI = M_PI;
 
 // Helper functions
 
+namespace {
+double constexpr sqrtNewtonRaphson(double x, double curr, double prev)
+{
+    return curr == prev ? curr : sqrtNewtonRaphson(x, 0.5 * (curr + x / curr), curr);
+}
+} // namespace
+
+/*
+* Constexpr version of the square root
+* Return value:
+*   - For a finite and non-negative value of "x", returns an approximation for the square root of "x"
+*   - Otherwise, returns NaN
+*/
+constexpr double sqrt(double x)
+{
+    assert(x >= 0);
+    assert(x < std::numeric_limits<double>::infinity());
+
+    if (x == 0) {
+        return 0;
+    }
+
+    return sqrtNewtonRaphson(x, x, 0);
+}
+
 constexpr double degrees_to_radians(double degrees)
 {
-    return degrees * pi / 180.0;
-}
-
-constexpr double ffmin(double a, double b)
-{
-    return std::min(a, b);
-}
-constexpr double ffmax(double a, double b)
-{
-    return std::max(a, b);
-}
-
-constexpr double clamp(double x, double min, double max)
-{
-    return std::clamp(x, min, max);
+    return degrees * PI / 180.0;
 }
 
 double random_double();
@@ -45,3 +56,5 @@ double random_double(double min, double max);
 std::string left_pad_string(std::string str, unsigned int length, char pad_char);
 std::string format_time(std::chrono::nanoseconds time);
 std::string format_time(long long time);
+
+} // namespace util
