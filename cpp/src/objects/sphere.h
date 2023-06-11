843e93573ccf08348c9a6a8df99aefdf8f0a09dd
Simon Danglmaier <simon@danglmaier.dev>
2023-06-11 17:57:12 +0200

    inline function

--- cpp/src/objects/sphere.h
+++ cpp/src/objects/sphere.h
@@ -17,7 +17,46 @@ class sphere : public hittable {
     }
 
   public:
-    bool hit(ray const& r, double t_min, double t_max, hit_record& rec) const override;
+    inline bool hit(ray const& r, double t_min, double t_max, hit_record& rec) const override
+    {
+        vec3 oc = r.origin() - center();
+
+        const auto a            = r.direction().length_squared();
+        const auto half_b       = dot(oc, r.direction());
+        const auto c            = oc.length_squared() - radius() * radius();
+        const auto discriminant = half_b * half_b - a * c;
+
+        if (discriminant > 0) {
+            const auto root = sqrt(discriminant);
+            auto       temp = (-half_b - root) / a;
+
+            if (temp < t_max && temp > t_min) {
+                rec.t = temp;
+                rec.p = r.at(rec.t);
+
+                vec3 outward_normal = (rec.p - center()) / radius();
+
+                rec.set_face_normal(r, outward_normal);
+                rec.mat_ptr = _mat_ptr;
+                return true;
+            }
+
+            temp = (-half_b + root) / a;
+
+            if (temp < t_max && temp > t_min) {
+                rec.t = temp;
+                rec.p = r.at(rec.t);
+
+                const vec3 outward_normal = (rec.p - center()) / radius();
+
+                rec.set_face_normal(r, outward_normal);
+                rec.mat_ptr = _mat_ptr;
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     constexpr vec3 center() const
     {
@@ -33,44 +72,3 @@ class sphere : public hittable {
     double                    _radius{0.0};
     std::shared_ptr<material> _mat_ptr;
 };
-
-bool sphere::hit(ray const& r, double t_min, double t_max, hit_record& rec) const
-{
-    vec3 oc = r.origin() - center();
-
-    const auto a            = r.direction().length_squared();
-    const auto half_b       = dot(oc, r.direction());
-    const auto c            = oc.length_squared() - radius() * radius();
-    const auto discriminant = half_b * half_b - a * c;
-
-    if (discriminant > 0) {
-        const auto root = sqrt(discriminant);
-        auto       temp = (-half_b - root) / a;
-
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.at(rec.t);
-
-            vec3 outward_normal = (rec.p - center()) / radius();
-
-            rec.set_face_normal(r, outward_normal);
-            rec.mat_ptr = _mat_ptr;
-            return true;
-        }
-
-        temp = (-half_b + root) / a;
-
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.at(rec.t);
-
-            const vec3 outward_normal = (rec.p - center()) / radius();
-
-            rec.set_face_normal(r, outward_normal);
-            rec.mat_ptr = _mat_ptr;
-            return true;
-        }
-    }
-
-    return false;
-}
